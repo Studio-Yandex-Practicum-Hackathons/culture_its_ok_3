e13820ab66991ce6be6efa0cdd397c513a7b950a
Sergey <san-8082@mail.ru>
2023-08-14 20:49:55 +0300

    develop init

--- culture_bot/excursion/models.py
+++ culture_bot/excursion/models.py
@@ -2,16 +2,22 @@ from django.db import models
 
 
 class Route(models.Model):
-    title = models.CharField(max_length=50,
-                             verbose_name="Название маршрута",)
-    description = models.TextField(verbose_name="Описание экспоната",)
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Название маршрута",
+    )
+    description = models.TextField(
+        verbose_name="Описание экспоната",
+    )
     route_map = models.ImageField(
         upload_to='excursion/router_map/',
         blank=True,
         verbose_name="Карта маршрута",
         help_text="Добавьте карту маршрута",
     )
-    rating = models.IntegerField(verbose_name="Рейтинг",)
+    rating = models.IntegerField(
+        verbose_name="Рейтинг",
+    )
 
     class Meta:
         verbose_name = "Маршрут"
@@ -30,21 +36,30 @@ class Exhibit(models.Model):
         related_name="exhibit",
         verbose_name="Маршрут",
     )
-    name = models.CharField(max_length=200,
-                            verbose_name="Название экспоната", )
-    description = models.TextField(verbose_name="Описание экспоната",)
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Название экспоната",
+    )
+    description = models.TextField(
+        verbose_name="Описание экспоната",
+    )
     image = models.ImageField(
         upload_to='excursion/exhibit/',
         blank=True,
         verbose_name="Картинка",
         help_text="Добавьте картинку экспоната",
     )
-    address = models.CharField(max_length=200,
-                               verbose_name="Адрес",)
-    rating = models.IntegerField(verbose_name="Рейтинг",)
-
-    # хотим ли мы создавать отдельную модель художников?
-    author = models.CharField(max_length=50, verbose_name="Художник",)
+    address = models.CharField(
+        max_length=200,
+        verbose_name="Адрес",
+    )
+    rating = models.IntegerField(
+        verbose_name="Рейтинг",
+    )
+    author = models.CharField(
+        max_length=50,
+        verbose_name="Художник",
+    )
 
     class Meta:
         verbose_name = "Экспонат"
@@ -55,17 +70,18 @@ class Exhibit(models.Model):
 
 
 class Reviews(models.Model):
-    # хотим ли мы хранить данные и создавать
-    # отдельную модель пользователя
-    # или ограничимся его подписью
-    author = models.CharField(max_length=20,
-                              verbose_name="Комментатор",)
+    author = models.CharField(
+        max_length=20,
+        verbose_name="Комментатор",
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата публикации",
     )
-    contact = models.CharField(max_length=50)
+    contact = models.CharField(
+        max_length=50
+    )
 
 
 class ReviewOnRoute(Reviews):
