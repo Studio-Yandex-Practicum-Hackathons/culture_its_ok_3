50c5cc0c787866bde1ed852d1e551368b070e8fe
George <83827848+webbzet@users.noreply.github.com>
2023-08-14 23:35:34 +0300

    Update create_bot.py

--- culture_bot/bot/create_bot.py
+++ culture_bot/bot/create_bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-TOKEN_API = '6560049240:AAGI8IQSekgA2BvhavLWJ9o6MUvHoAtLA7I'
+TOKEN_API = '#'
 
 storage = MemoryStorage()
 
