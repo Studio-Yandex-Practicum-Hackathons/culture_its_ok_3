7284467b422ddb19caf0cd5419a39f5fb7d9af4f
ragozindenis <Ragozin3Dm@yandex.ru>
2023-08-30 02:35:26 -0300

    fixed add data to db and add route review and rating

--- culture_bot/api/urls.py
+++ culture_bot/api/urls.py
@@ -1,16 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (CreateGoogleGeneralReportViewSet, ExhibitCommentViewSet,
-                       ExhibitViewSet, RouteReviewViewSet, RouteViewSet,
-                       UserFeedbackViewSet)
+from api.views import CreateGoogleGeneralReportViewSet
+
 
 router = DefaultRouter()
-router.register(r'user-feedback', UserFeedbackViewSet, basename='user-feedback')
-router.register(r'exhibit-comment', ExhibitCommentViewSet)
-router.register(r'route-review', RouteReviewViewSet)
-router.register(r'route', RouteViewSet)
-router.register(r'exhibit', ExhibitViewSet)
 router.register(
     r'create-google-report',
     CreateGoogleGeneralReportViewSet,
