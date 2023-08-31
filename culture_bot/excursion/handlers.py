4528a8f8cea9789080ca9f59c162563f0d6b041d
ragozindenis <Ragozin3Dm@yandex.ru>
2023-08-31 17:11:19 -0300

    fixed

--- culture_bot/excursion/handlers.py
+++ culture_bot/excursion/handlers.py
@@ -237,7 +237,6 @@ async def set_rating_exhibit(message: Message, state: FSMContext):
                 await message_answer(
                     message,
                     question_end,
-                    reply_markup=keyboard_menu
                 )
 
 
